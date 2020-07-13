582efd12722f9aa15f10bbad1f67e6d793b8b99c
ravish510 <ravishankar.rcc@gmail.com>
2020-07-13 17:33:52 +0530

    modifying send1.py

--- send1.py
+++ send1.py
@@ -4,10 +4,14 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-
+channel.queue_declare(queue='bye')
 for i in range(10):
     channel.basic_publish(exchange='',
                           routing_key='hello',
+                          body='Hello I am Ravi Shankar!')
+for j in range(10):
+	channel.basic_publish(exchange='',
+                          routing_key='bye',
                           body='Hello I am Ravi Shankar!')
 # print(" [x] Sent 'Hello World!'")
 
